917ac8d48778066d3fe308f9b9b65e445272cbde
Snus292 <georgiigiblov@gmail.com>
2024-05-28 14:09:47 +0300

    исправлены ошибки в маршрутах и представлениях

--- routes/add.js
+++ routes/add.js
@@ -1,7 +1,8 @@
 const{Router} = require("express")
+const Course =require("../models/course")
 const router = Router()
 
-router.get("/add",(req,res)=> {
+router.get("/",(req,res)=> {
     res.render("add",{
         title: "Add a course",
         isAdd: true
@@ -11,7 +12,7 @@ router.get("/add",(req,res)=> {
 router.post("/", async(req,res)=>{
     const course =new Course(req.body.title,req.body.price, req.body.img)
     await course.save()
-    
+
     res.redirect("/courses")
 })
 module.exports= router
