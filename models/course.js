0e04502211a23ffccb9b21d810e387bb60d9bcf0
Snus292 <georgiigiblov@gmail.com>
2024-06-04 14:12:53 +0300

    добавленны функции редактирования в моделях, оптимизированы шаблоны и настроенные ссылки

--- models/course.js
+++ models/course.js
@@ -1,6 +1,6 @@
-const { v4: uuid } = require('uuid');
-const fs = require('fs');
-const path = require('path');
+const { v4: uuid } = require("uuid");
+const fs = require("fs");
+const path = require("path");
 
 class Course {
   constructor(title, price, img) {
@@ -25,7 +25,7 @@ class Course {
 
     return new Promise((resolve, reject) => {
       fs.writeFile(
-        path.join(__dirname, '..', 'data', 'courses.json'),
+        path.join(__dirname, "..", "data", "courses.json"),
         JSON.stringify(courses, null, 2),
         (err) => {
           if (err) {
@@ -38,20 +38,20 @@ class Course {
     });
   }
 
-  static async update(course) {
-    const courses = await Course.getAll();
-    const idx = courses.findIndex((c) => c.id === course.id);
-    courses[idx] = course;
+  static async update(course) { // Объявляет статический асинхронный метод update, который принимает один параметр - объект course.
+    const courses = await Course.getAll(); // Используем await для ожидания выполнения асинхронного метода getAll() класса Course, который возвращает список всех курсов. Результат сохраняется в переменную courses.
+    const idx = courses.findIndex((c) => c.id === course.id); // Находим индекс курса в массиве courses, идентификатор которого совпадает с идентификатором обновляемого курса.
+    courses[idx] = course; // Заменяет старый курс новым в массиве courses по найденному индексу.
 
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, '..', 'data', 'courses.json'),
-        JSON.stringify(courses, null, 2),
-        (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
+    return new Promise((resolve, reject) => { // Возвращаем новое обещание (Promise), которое будет завершено при записи обновленного списка курсов в файл.
+      fs.writeFile( // Метод fs.writeFile записывает данные в файл. Если файла нет, он будет создан.
+        path.join(__dirname, "..", "data", "courses.json"), // Указываем путь к файлу, в который будет произведена запись. Используем path.join для создания пути к файлу, чтобы он был корректным на всех операционных системах.
+        JSON.stringify(courses, null, 2), // Преобразуем объект courses в строку формата JSON. Параметры null и 2 используются для форматирования JSON с отступами для удобного чтения.
+        (err) => { // Колбэк-функция, которая вызывается после завершения записи в файл.
+          if (err) { // Если произошла ошибка при записи в файл, выполняется эта ветка кода.
+            reject(err); // Отклоняет обещание и передаем ошибку в функцию reject.
+          } else { // Если запись в файл прошла успешно, выполняется эта ветка кода.
+            resolve(); // Завершает обещание успешно, вызывая функцию resolve.
           }
         }
       );
@@ -61,8 +61,8 @@ class Course {
   static getAll() {
     return new Promise((resolve, reject) => {
       fs.readFile(
-        path.join(__dirname, '..', 'data', 'courses.json'),
-        'utf-8',
+        path.join(__dirname, "..", "data", "courses.json"),
+        "utf-8",
         (err, content) => {
           if (err) {
             reject(err);
@@ -74,9 +74,9 @@ class Course {
     });
   }
 
-  static async getById(id) {
-    const courses = await Course.getAll();
-    return courses.find((course) => course.id === id);
+  static async getById(id) { // Объявление статического асинхронного метода getById в классе. Статический метод доступен на уровне класса и не требует создания экземпляра класса., принимает один параметр - id
+    const courses = await Course.getAll(); // await для ожидания выполнения асинхронного метода getAll() класса Course, который должен вернуть список всех курсов. Результат сохраняется в переменную courses.
+    return courses.find((course) => course.id === id); // Метод find перебирает все элементы массива courses и возвращает первый элемент, удовлетворяющий условию course.id === id. Если такой элемент не найден, возвращает undefined.
   }
 }
 
