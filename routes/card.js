059f4ef7991f95d792e550652491e81b0f3c46af
Snus292 <georgiigiblov@gmail.com>
2024-06-05 08:34:41 +0300

    реализованы другие компоненты корзины, добавленны маршрут и представление ,применены условия к фукции добавления в корзину

---
+++ routes/card.js
@@ -0,0 +1,19 @@
+const {Router} = require("express")
+const Card = require("../models/card")
+const Course = require("../models/course")
+const router = Router()
+
+router.post("/add",async(res,req)=>{
+    const course = await Course.getById(req.body.id)
+    await Card.add(course)
+    res.redirect("/card")
+})
+
+router.get("/",async(req,res)=>{
+    const card = await Card.fetch()
+    res.render("card",{
+        title: "Card",
+        card
+    })
+})
+module.exports = router
